24e029555090b354a9493e2269da87d75d5c0f84
You don't know me <jmw150@hotmail.com>
2021-05-11 12:46:14 -0400

    mainTue May 11 11:20:49 EDT 2021

---
+++ codes.py
@@ -0,0 +1,14 @@
+# libraries
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+import sympy as sp
+from sympy.abc import * # skip declaring symbols, eats up namespace though
+#!pip install magpylib
+import magpylib as em
+ 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
